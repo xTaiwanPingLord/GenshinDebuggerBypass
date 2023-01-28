fb509a19057c3e56590433061478911b6914e29b
Alukym <100074184+Alukym@users.noreply.github.com>
2023-01-28 20:05:02 +0800

    fix mhyprot3 close

---
+++ source/CloseMhyprot3.h
@@ -0,0 +1,2 @@
+#pragma once
+bool CloseHandleByName(const wchar_t* name);
--- source/main.cpp
+++ source/main.cpp
@@ -1,38 +1,38 @@
-#pragma once
+ï»¿#pragma once
 #include "DebuggerBypass.h"
+#include "CloseMhyprot3.h"
 
 #include <stdio.h>
 #include <format>
-#include "CloseHandleByName.h"
 #define LOG(fmtstr, ...) printf("%s\n", std::format(fmtstr, ##__VA_ARGS__).c_str());
 
-void RunMain(HMODULE *phModule)
+void RunMain(HMODULE* phModule)
 {
 	// New Console
 	AllocConsole();
-	freopen_s((FILE **)stdin, "CONIN$", "r", stdin);
-	freopen_s((FILE **)stdout, "CONOUT$", "w", stdout);
-	freopen_s((FILE **)stderr, "CONOUT$", "w", stderr);
+	freopen_s((FILE**)stdin, "CONIN$", "r", stdin);
+	freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+	freopen_s((FILE**)stderr, "CONOUT$", "w", stderr);
 	SetConsoleOutputCP(CP_UTF8);
 
 	DebuggerBypassPre();
 
-	while (GetModuleHandle(L"UserAssembly.dll") == nullptr)
-	{
-		LOG("[INFO] UserAssembly.dll isn't initialized. Waiting for 2 seconds.");
+	while (GetModuleHandle(L"UserAssembly.dll") == nullptr) {
+		LOG("[INFO] UserAssembly.dll isn't initialized. Waiting for 2 second.");
 		Sleep(2000);
 	}
 
 	DebuggerBypassPost();
 
-	LOG("[INFO] Debug bypass complete. You can inject other dll now. Happy debugging!");
+	LOG("[INFO] Debug bypass complete.");
 
-	//ÕâÀï²»¼ÓÀ¨ºÅ±àÒë¹ı²»ÁË Éµ±Æ±àÒëÆ÷ ¼ÓÁË¿´×ÅÄÑÊÜ°¡°¡°¡°¡°¡°¡°¡
+	//è¿™é‡Œä¸åŠ å¤§æ‹¬å·ç¼–è¯‘è¿‡ä¸äº† ğŸ–•Fuck C++ğŸ–• ğŸ–•Fuck MicrosoftğŸ–•
 	if (CloseHandleByName(L"\\Device\\mhyprot2")) {
-		LOG("[INFO] The Mhyprot2 handle was successfully closed. Happy hacking!");
+		LOG("[INFO] Mhyprot3 was closed successfully.");
 	}
-	else
-		LOG("[ERROR] Failed to close mhyprot2 handle. Report this Issue and describe it.");
+	else {
+		LOG("[ERROR] Failed to close mhyprot3 handle.");
+	}
 
 	return;
 }
